b6e3fa927f0fcf028dc0367e8232a8cc2c94fe42
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2023-12-21 20:10:46 +0400

    Add files via upload

---
+++ zadacha11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class zadacha11 {
+public static void main(String[] arg){
+float r=7.5F;
+float PI=3.14F;
+Scanner sc=new Scanner(System.in);
+double p=2*Math.PI*r;
+System.out.println("Perimetr is =" + p);
+double s=Math.PI*r*r;
+System.out.println("Area is =" + s);
+}
+
+}
