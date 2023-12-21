ebc30b91828a070b0f9b48a87a98d1081e823dee
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2023-12-21 20:11:25 +0400

    Update zadacha11.java

--- zadacha11.java
+++ zadacha11.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 public class zadacha11 {
-public static void main(String[] arg){
-float r=7.5F;
-float PI=3.14F;
-Scanner sc=new Scanner(System.in);
-double p=2*Math.PI*r;
-System.out.println("Perimetr is =" + p);
-double s=Math.PI*r*r;
-System.out.println("Area is =" + s);
-}
+  public static void main(String[] arg) {
+    float r = 7.5 F;
+    float PI = 3.14 F;
+    Scanner sc = new Scanner(System.in);
+    double p = 2 * Math.PI * r;
+    System.out.println("Perimetr is =" + p);
+    double s = Math.PI * r * r;
+    System.out.println("Area is =" + s);
+  }
 
 }
